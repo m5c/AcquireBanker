7b7f7f2bb733c7a9d472c32b98761f71ce6ad80c
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2023-11-06 17:53:54 -0500

    Added Spring Launcher

---
+++ src/main/java/eu/kartoffelquadrat/acquirebanker/SpringLauncher.java
@@ -0,0 +1,15 @@
+package eu.kartoffelquadrat.acquirebanker;
+
+import static eu.kartoffelquadrat.acquirebanker.Main.main;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLauncher {
+
+  // This literally just starts up the old launcher.
+  public static void main(String[] args) {
+    Main.main(new String[] {});
+  }
+
+}
